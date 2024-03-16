0fcb6148a5dcf07ba151380fb403ee6c551bcd45
justin jolly <justinjolly355@gmail.com>
2024-03-16 22:35:39 +0530

    ji

--- lib/pages/friends.dart
+++ lib/pages/friends.dart
@@ -1,10 +1,4 @@
 import 'package:flutter/material.dart';
-
-
-
-
-
-
 enum FriendFilter { All, IOwe, OweMe }
 
 class FriendsPage extends StatefulWidget {
@@ -96,20 +90,7 @@ class _FriendsPageState extends State<FriendsPage> {
           ),
         ),
         centerTitle: true, // Center the title
-        leading: IconButton(
-          icon: Icon(Icons.arrow_back),
-          onPressed: () {
-            Navigator.pop(context);
-          },
-        ),
-        actions: [
-          IconButton(
-            icon: Icon(Icons.person_add, size: 24.0),
-            onPressed: () {
-              // Add Friend functionality here
-            },
-          ),
-        ],
+        automaticallyImplyLeading: false,
       ),
       body: Column(
         children: [
