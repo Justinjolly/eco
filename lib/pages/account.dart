b40ebb9a2086f1c65a50e3f54d7308c62570c71c
Dony <donytomythayyil@gmail.com>
2024-03-16 22:57:12 +0530

    aa

--- lib/pages/account.dart
+++ lib/pages/account.dart
@@ -1,3 +1,4 @@
+import 'package:app/main.dart';
 import 'package:flutter/material.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
@@ -217,7 +218,12 @@ class _AccountPageState extends State<AccountPage> {
               'Logout',
               style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
             ),
-            onTap: () {},
+            onTap: () {
+              Navigator.push(
+                context,
+                MaterialPageRoute(builder: (context) => MyHomePage()),
+              );
+            },
           ),
         ],
       ),
