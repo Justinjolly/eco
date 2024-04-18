0422a5c5d23b9996f8e867eb174151334e4177eb
Jibbin Jacob Daniel <jibbinjacobdaniel2024@cs.sjcetpalai.ac.in>
2024-04-18 15:17:53 +0530

    fi

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:app/pages/homepage.dart';
+import 'package:app/pages/navbar.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
