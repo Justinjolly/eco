159301387fbceb6dbdcc29bc265ab379651f5d0b
Jibbin Jacob Daniel <jibbinjacobdaniel2024@cs.sjcetpalai.ac.in>
2024-04-21 23:29:16 +0530

    gg

--- lib/pages/group.dart
+++ lib/pages/group.dart
@@ -1,3 +1,4 @@
+import 'package:app/pages/amount.dart';
 import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
@@ -434,35 +435,44 @@ class SplitAmountCard extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Card(
-      elevation: 2.0,
-      shape: RoundedRectangleBorder(
-        borderRadius: BorderRadius.circular(8.0),
-      ),
-      child: Padding(
-        padding: EdgeInsets.all(12.0),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          mainAxisSize: MainAxisSize.min,
-          children: <Widget>[
-            Text(
-              'Split Amount',
-              style: TextStyle(
-                fontWeight: FontWeight.bold,
-                fontSize: 16.0,
+    return GestureDetector(
+      onTap: () {
+        Navigator.push(
+          context,
+          MaterialPageRoute(
+            builder: (context) => TripDetailsPage(), // Replace AnotherPage with the desired page
+          ),
+        );
+      },
+      child: Card(
+        elevation: 2.0,
+        shape: RoundedRectangleBorder(
+          borderRadius: BorderRadius.circular(8.0),
+        ),
+        child: Padding(
+          padding: EdgeInsets.all(12.0),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            mainAxisSize: MainAxisSize.min,
+            children: <Widget>[
+              Text(
+                'Split Amount',
+                style: TextStyle(
+                  fontWeight: FontWeight.bold,
+                  fontSize: 16.0,
+                ),
               ),
-            ),
-            SizedBox(height: 8.0),
-            Text(
-              'Total Amount: ₹${totalAmount.toStringAsFixed(2)}',
-              style: TextStyle(
-                fontSize: 14.0,
+              SizedBox(height: 8.0),
+              Text(
+                'Total Amount: ₹${totalAmount.toStringAsFixed(2)}',
+                style: TextStyle(
+                  fontSize: 14.0,
+                ),
               ),
-            ),
-          ],
+            ],
+          ),
         ),
       ),
     );
   }
 }
-
