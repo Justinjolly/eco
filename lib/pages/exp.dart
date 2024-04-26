a2e29cb1f0b776e0b20bb6eaf64fb7667dfa5f64
Jibbin Jacob Daniel <jibbinjacobdaniel2024@cs.sjcetpalai.ac.in>
2024-04-26 10:12:25 +0530

    exp

---
+++ lib/pages/exp.dart
@@ -0,0 +1,42 @@
+import 'package:flutter/material.dart';
+import 'package:razorpay_flutter/razorpay_flutter.dart';
+import 'razor.dart'; // Import your PaymentPage widget
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Payment App',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: HomePage(), // Set your home page
+      routes: {
+        '/payment': (context) => PaymentPage(), // Add PaymentPage to routes
+      },
+    );
+  }
+}
+
+class HomePage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Home'),
+      ),
+      body: Center(
+        child: ElevatedButton(
+          onPressed: () {
+            Navigator.pushNamed(context, '/payment'); // Navigate to PaymentPage
+          },
+          child: Text('Go to Payment'),
+        ),
+      ),
+    );
+  }
+}
